81b2053379e64fd39a2285a5c3cfc2fae73a5aa2
Bouabacar Camara <boubacarbakarycamara@esp.sn>
2025-11-05 15:33:31 +0000

    modification app

--- src/App.tsx
+++ src/App.tsx
@@ -12,29 +12,62 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    // <div className="md:h-full md:w-48 ">
-    <div className="sm-640 md-768 lg-1024 xl-1280 2xl-1536">
-      <div className="md:h-full md:w-48 ">
-       <div className="p-5 md:px-[15%]">
+
+
+
+    <div className="w-full min-h-screen bg-white text-gray-900">
+      {/* Barre de navigation et section d'accueil */}
+      <div className="px-4 sm:px-8 md:px-[10%] lg:px-[15%] py-6">
         <Navbar />
         <Home />
       </div>
 
-      <About />
+      {/* Section "À propos" */}
+      <section className="px-4 sm:px-8 md:px-[10%] lg:px-[15%] py-8">
+        <About />
+      </section>
 
-      <div className="p-5 md:px-[15%]">
-       <Experiences />  
-       <Projects />
-      
-      </div>
-       <div>
-        {/* <Access /> */}
-       </div>
-        <Footer /> 
-        </div>
+      {/* Expériences et projets */}
+      <section className="px-4 sm:px-8 md:px-[10%] lg:px-[15%] py-8">
+        <Experiences />
+        <Projects />
+      </section>
+
+      {/* Section optionnelle */}
+      {/* <section className="px-4 sm:px-8 md:px-[10%] lg:px-[15%] py-8">
+    <Access />
+  </section> */}
+
+      {/* Pied de page */}
+      <footer className="px-4 sm:px-8 md:px-[10%] lg:px-[15%] py-6 bg-gray-100">
+        <Footer />
+      </footer>
     </div>
-     
-      
+
+
+    // <div className="md:h-full md:w-48 ">
+    // <div className="sm-640 md-768 lg-1024 xl-1280 2xl-1536">
+    //   <div className="md:h-full md:w-48 ">
+    //    <div className="p-5 md:px-[15%]">
+    //     <Navbar />
+    //     <Home />
+    //   </div>
+
+    //   <About />
+
+    //   <div className="p-5 md:px-[15%]">
+    //    <Experiences />  
+    //    <Projects />
+
+    //   </div>
+    //    <div>
+    //     {/* <Access /> */}
+    //    </div>
+    //     <Footer /> 
+    //     </div>
+    // </div>
+
+
     // </div>
   )
 }
